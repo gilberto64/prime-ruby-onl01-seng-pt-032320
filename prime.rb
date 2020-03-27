44a3d3ae8f83b63c48c26a01556fe6b3a6c274cf
Gilberto Ocampo <ocampo@gate.net>
2020-03-27 17:39:50 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,17 +1,17 @@
 def prime?(num)
-  # counter = 0  
-  #while counter < num.length  
-  if num < 2  
-   false
-  elsif num == 2 || num == 3 
-     true 
-  elsif num.even?
-    false 
-  elsif num % 2 == 0 || num % 3 == 0 || num % 5 == 0 || num % 7 == 0 || num % 11 == 0 || num % 13 == 0 || 
-    true 
-  else 
-    true 
-  end
-  # counter = counter + 1
+  counter = 0  
+  while counter < num.length  
+    if num < 2  
+    false
+    elsif num == 2 || num == 3 
+       true 
+    elsif num.even?
+      false 
+    elsif num % 2 == 0 || num % 3 == 0 || num % 5 == 0 || num % 7 == 0 || num % 11 == 0 || num % 13 == 0 || 
+      true 
+    else 
+      true 
+    end
+    counter = counter + 1
 end 
 
