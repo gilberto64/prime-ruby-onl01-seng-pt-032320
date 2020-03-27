27e99dd0e9461ebb9840fb0699a2d67ab02e2dc7
Gilberto Ocampo <ocampo@gate.net>
2020-03-27 18:17:24 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,14 @@
 def prime?(num)
   (num).to_i 
   if num < 2 
-    puts false 
+    puts "false" 
   elsif num == 2 || num == 3
-    puts true   
+    puts "true"   
   elsif num.even?
-    puts false 
+    puts "false"  
   elsif num % 5 == 0 || num % 7 == 0 || num % 11 == 0 || num % 13 == 0 || 
-     puts false
+     puts "false" 
   else 
-    false 
+    "false"  
   end 
 end 
