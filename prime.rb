3425d9218c81c14a958c0ca3692257614e9144f4
Gilberto Ocampo <ocampo@gate.net>
2020-03-27 18:38:04 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,21 @@
-def prime?(num)
-  num.to_i 
-  if num < 2 
-    return false 
-  elsif num == 2 || num == 3
-    return true   
-  elsif num.even?
-    return false 
-  elsif num % 3 == 0 || num % 5 == 0 || num % 7 == 0 || num % 11 == 0 || num % 13 == 0 || num % 17 == 0 || num % 19 == 0
-     return false 
-  else 
-    return false 
-  end 
-end 
+# def prime?(num)
+#   num.to_i 
+#   if num < 2 
+#     return false 
+#   elsif num == 2 || num == 3
+#     return true   
+#   elsif num.even?
+#    return false 
+#  elsif num % 3 == 0 || num % 5 == 0 || num % 7 == 0 || num % 11 == 0 || num % 13 == 0 || num % 17 == 0 || num % 19# == 0
+    # return false 
+  # else 
+  #  return false 
+  # end 
+# end 
 
+def prime?(num)
+  (2..(num - 1)).each do |n|
+    return false if num % n == 0
+  end
+  true
+end
